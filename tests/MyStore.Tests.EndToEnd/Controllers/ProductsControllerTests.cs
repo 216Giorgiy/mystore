c458777abaa53c504d06b800f44aa7045eaf3cc8
Piotr Gankiewicz <spetz90@gmail.com>
2018-06-17 14:37:14 +0200

    Tests

---
+++ tests/MyStore.Tests.EndToEnd/Controllers/ProductsControllerTests.cs
@@ -0,0 +1,23 @@
+﻿using System.Net.Http;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.TestHost;
+using MyStore.Api;
+
+namespace MyStore.Tests.EndToEnd.Controllers
+{
+    public class ProductsControllerTests
+    {
+        private readonly TestServer _server;
+        private readonly HttpClient _client;
+
+        public ProductsControllerTests()
+        {
+            _server = new TestServer(WebHost.CreateDefaultBuilder()
+                .UseStartup<Startup>()
+                .UseEnvironment("test"));
+
+            _client = _server.CreateClient();
+        }
+    }
+}
